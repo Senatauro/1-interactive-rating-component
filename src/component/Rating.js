adf6be5b106dc836cd2ebb6ab374ceb40bc4d17e
Senatauro <cgsste@hotmail.com>
2022-06-15 21:47:36 -0300

    minor update
    no unused import

--- src/component/Rating.js
+++ src/component/Rating.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { reduxRating } from "../redux/ratingSlice";
 
 import RatingButton from "./RatingButton";
